7f343c527b762cd52367b5a5e1894d5bcd7a7275
Bentroen <bemcdc@gmail.com>
2023-05-21 14:30:47 -0300

    Adicionar algoritmo de Prim

--- prim.py
+++ prim.py
@@ -38,11 +38,52 @@ def plot_graph(graph: list):
     return G
 
 
+def find_mst_prim(graph: list):
+    """
+    Returns the minimum spanning tree of the given graph using Prim's algorithm.
+    """
+
+    node_selected = [False for i in range(len(graph))]
+    no_edge = 0
+    node_selected[0] = True
+
+    N = len(graph)
+    minimum = float("-inf")
+    a = 0
+    b = 0
+
+    mst = [[0 for i in range(N)] for j in range(N)]
+
+    while no_edge < N - 1:
+        minimum = float("inf")
+        a = 0
+        b = 0
+        for m in range(N):
+            if node_selected[m]:
+                for n in range(N):
+                    if (not node_selected[n]) and graph[m][n]:
+                        # Not in selected edges and there is an edge
+                        if minimum > graph[m][n]:
+                            minimum = graph[m][n]
+                            a = m
+                            b = n
+                            print(a, b)
+        print(f"Edge {no_edge}: ({a}, {b}) cost: {minimum}")
+        mst[a][b] = minimum
+
+        node_selected[b] = True
+        no_edge += 1
+
+    return mst
+
+
 def main():
     graph = load_graph_from_file(INPUT_PATH)
     print(graph)
 
     plot_graph(graph)
+    mst = find_mst_prim(graph)
+    plot_graph(mst)
 
 
 if __name__ == "__main__":
