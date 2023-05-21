5799b973f13c641ba65d8400c8e771d1519564e9
Bentroen <bemcdc@gmail.com>
2023-05-21 13:38:15 -0300

    Adicionar código para plotagem do grafo

--- prim.py
+++ prim.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+
 INPUT_PATH = "prim.txt"
 
 
@@ -16,9 +20,29 @@ def load_graph_from_file(path: str):
     return graph
 
 
+def plot_graph(graph: list):
+    print(graph)
+    G = nx.from_numpy_array(np.array(graph), parallel_edges=False)
+
+    # Plot it
+    nx.draw(G, with_labels=True)
+    pos = nx.spring_layout(G)
+    labels = nx.get_edge_attributes(G, "weight")
+
+    print("pos:", pos)
+    print("labels:", labels)
+
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    plt.show()
+
+    return G
+
+
 def main():
     graph = load_graph_from_file(INPUT_PATH)
     print(graph)
+
+    plot_graph(graph)
 
 
 if __name__ == "__main__":
